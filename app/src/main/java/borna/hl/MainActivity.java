429e4d2da1c754c7bb90b83427038a2d88c073d0
unknown_device <stimacakaburek@gmail.com>
2017-06-18 14:21:00 +0200

    working show map activity

--- app/src/main/java/borna/hl/MainActivity.java
+++ app/src/main/java/borna/hl/MainActivity.java
@@ -3,11 +3,21 @@ package borna.hl;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.maps.MapView;
 
+public class MainActivity extends Activity {
+    private MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Mapbox.getInstance(this,getString(R.string.access_token));
         setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+
     }
 }
